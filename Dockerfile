608cdff21ee8791c67bcf1a2b522893c09655804
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-18 13:48:43 -0300

    coneccion con socket labs doceker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM  python:3.9.19-alpine3.19
-
+FROM python:3.9.19-alpine3.19
 
 WORKDIR /app
 
 COPY ./ . 
 
 RUN pip3 --no-cache-dir install -r ./requirements.txt
+RUN pip3 --no-cache-dir install gunicorn
+
 ENV FLASK_ENV=production
-CMD ["python3","run.py"]
+
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:4000", "run:app"]
--- run.py
+++ run.py
@@ -1,8 +1,14 @@
-from app import create_app,  socketio
+# from app import create_app,  socketio
 
-app= create_app()
+# app= create_app()
 
+
+# if __name__ == "__main__":
+#     socketio.run(app,host="0.0.0.0",port=4000,debug=True,allow_unsafe_werkzeug=True)
+    
+from app import create_app, socketio
+
+app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app,host="0.0.0.0",port=4000,debug=True,allow_unsafe_werkzeug=True)
-    
+    socketio.run(app, host="0.0.0.0", port=4000)
