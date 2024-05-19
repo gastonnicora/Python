c84ea93436ef08dda332d73a786a56a5172826f3
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-18 21:32:28 -0300

    logs

--- run.py
+++ run.py
@@ -4,15 +4,28 @@ from os import environ
 from dotenv import load_dotenv
 import logging
 
+from logging.handlers import RotatingFileHandler
+from flask import  send_file
+
 
 env = environ.get("FLASK_ENV", "development")
 
 load_dotenv()
 app= create_app()
 
+# Configuración del manejador de registros para escribir en un archivo
+file_handler = RotatingFileHandler('app.log', maxBytes=1024 * 1024 * 10, backupCount=10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.DEBUG)  # Establecer el nivel de registro al más bajo
+app.logger.addHandler(file_handler)
+
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 app.logger.addHandler(console_handler)
+@app.route('/logs')
+def serve_logs():
+    log_file_path = 'app.log'  # Ruta al archivo de registro
+    return send_file(log_file_path, as_attachment=True)
 
 if __name__ == "__main__":
      
