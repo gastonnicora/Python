9f93817786587a93e2014f379b60163f2268132c
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-19 20:36:57 -0300

    prueba correccion de celery

--- app/helpers/celery.py
+++ app/helpers/celery.py
@@ -3,17 +3,22 @@ from os import environ
 import requests as R   
 
 
-celery=environ.get("CELERY", "127.0.0.1:5000")
-
+celery_url= "http://"+environ.get("CELERY", "127.0.0.1:5000")
      
 def deleteConfirm(uuid):
-    r=R.get(f"http://celery:5000/deleteConfirm/{uuid}")
-    if r.status_code == 202:
-        print("La solicitud se realizó correctamente.")
-        print("Respuesta:", r.json())
-    else:
-        print("Hubo un problema al realizar la solicitud a Celery.")
-        print("Código de estado:", r.status_code)
+    try:
+        # Hacer una solicitud GET al servicio de Celery
+        response = R.get(f"{celery_url}/deleteConfirm/{uuid}")
+        
+        # Verificar si la solicitud fue exitosa (código de estado 200)
+        if response.status_code == 200:
+            print("Solicitud exitosa a Celery")
+            print("Contenido de la respuesta:", response.text)
+        else:
+            print("La solicitud a Celery falló. Código de estado:", response.status_code)
+    
+    except Exception as e:
+        print("Error al hacer la solicitud a Celery:", str(e))
 
 def finishedArticle(uuid,time):
     data= {"article":uuid,"time":time}
