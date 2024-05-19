51a9889cb3f86422b351596f002e0e58f62bdb73
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-19 19:14:27 -0300

    cambios en celery

--- app/helpers/celery.py
+++ app/helpers/celery.py
@@ -10,11 +10,8 @@ import socket
 
 from app.helpers.saveSession import loadDict
 
-celery=environ.get("CELERY", "127.0.0.1:5000")
 
 
-# Obtener la URL del servidor para el puerto 4000 (por ejemplo)
-server_url = "http://0.0.0.0:4000"
 
 def url(referer):
     r=None
@@ -30,83 +27,25 @@ def url(referer):
         except:
             return None,404
 
-def login():
-    uuid=str(uuid4())
-    Celery().setUuid(uuid)
-    token = jwt.encode({'uuid':uuid},environ.get("SECRET_KEY","1234"), algorithm="HS256")
-    link,r= url(celery)
-    if r== 404:
-        return jsonify({"error":"La url esta mal o el servidor desconectado"}),404  
-    Celery().setLink(link)
-    headers = {'Referer': server_url,"X-Access-Tokens":token}
-    r=R.get(link+"/login",headers=headers)
+celery,r=url(environ.get("CELERY", "127.0.0.1:5000"))
 
      
 def deleteConfirm(uuid):
-    if Celery()._uuid== None:
-        login()
-    link= Celery().getLink()
-    headers = {'Referer': server_url}
-    r=R.get(link+"/deleteConfirm/"+uuid,headers=headers)
+    r=R.get(celery+"/deleteConfirm/"+uuid)
 
 def finishedArticle(uuid,time):
-    if Celery()._uuid== None:
-        login()
-    link= Celery().getLink()
-    headers = {'Referer': server_url}
     data= {"article":uuid,"time":time}
-    r=R.post(link+"/finishedArticle",headers=headers,json=data)
+    r=R.post(celery+"/finishedArticle",json=data)
 
 
 def startedArticle(uuid,time):
-    if Celery()._uuid== None:
-        login()
-    link= Celery().getLink()
-    headers = {'Referer': server_url}
     data= {"article":uuid,"time":time}
-    r=R.post(link+"/startedArticle",headers=headers,json=data)
+    r=R.post(celery+"/startedArticle",json=data)
 
 
 def startedAuction(uuid,time):
-    if Celery()._uuid== None:
-        login()
-    link= Celery().getLink()
-    headers = {'Referer': server_url}
     data= {"article":uuid,"time":time}
-    r=R.post(link+"/startedAuction",headers=headers,json=data)
+    r=R.post(celery+"/startedAuction",json=data)
 
 
 
-
-class Celery:
-    _instance = None
-    _uuid=None
-    _link=None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            data= loadDict("Celery.pkl")
-            if data:
-                cls._uuid= data["uuid"]  
-                cls._link= data["link"] 
-        return cls._instance
-
-    def __init__(cls):
-        cls.variable = "Soy un Singleton"
-    
-    def _getUuid(cls):
-        return cls._uuid
-    
-    def setUuid(cls, uuid):
-        cls._uuid=uuid
-
-    def setLink(cls,link):
-        cls._link= link
-    
-    def getLink(cls):
-        return cls._link
-    
-    def toDict(cls):
-        data={"uuid":cls._uuid,"link":cls._link}
-        return data 
