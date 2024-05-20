57ab4479331852bfd6372f6fb1aa6d2afc0122d6
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-19 22:07:06 -0300

    coneccion celery

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import os
 from app import create_app,  socketio
 from os import environ
 from dotenv import load_dotenv
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 env = environ.get("FLASK_ENV", "development")
@@ -10,7 +9,6 @@ env = environ.get("FLASK_ENV", "development")
 load_dotenv()
 app= create_app()
 
-app.wsgi_app = ProxyFix(app.wsgi_app, x_host=1)
 
 
 if __name__ == "__main__":
