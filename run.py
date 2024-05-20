bbf029abbb6cea259db4cd24bf49c1ebd67dff64
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-05-19 21:32:32 -0300

    nginx

--- run.py
+++ run.py
@@ -2,12 +2,15 @@ import os
 from app import create_app,  socketio
 from os import environ
 from dotenv import load_dotenv
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 env = environ.get("FLASK_ENV", "development")
 
 load_dotenv()
 app= create_app()
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_host=1)
 
 
 if __name__ == "__main__":
