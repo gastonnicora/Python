2f34976bb766008558658dca9147fe6d61bb1c63
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2024-06-13 19:08:53 -0300

    cambios token

--- app/helpers/celery.py
+++ app/helpers/celery.py
@@ -9,21 +9,27 @@ from app.helpers.sessions import Sessions
 redis_host = environ.get("REDIS_HOST", "localhost")
 redis_client = redis.Redis(host=redis_host, port=6379)
 
-celery={"uuid":str(uuid.uuid4())}
-print("secret")
-print(environ.get("SECRET_KEY","1234"))
-
-id,session =Sessions().addSession(celery)
-token = jwt.encode({'uuid':id}, environ.get("SECRET_KEY","1234"), algorithm="HS256")
-
-print("token celery")
-print(token) 
+class Token:
+    _instance = None
+    _token= None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            celery={"uuid":str(uuid.uuid4())}
+            id,session =Sessions().addSession(celery)
+            cls._token=jwt.encode({'uuid':id}, environ.get("SECRET_KEY","1234"), algorithm="HS256")
+        return cls._instance
+    @classmethod
+    def getToken(cls):
+        print("secret")
+        print(environ.get("SECRET_KEY","1234"))
+        return cls._token
 
 def deleteConfirm(uuid):
     message = json.dumps({
         'task_name': "deleteConfirm",
         'uuid': uuid,
-        'token':token
+        'token':Token().getToken()
     })
     redis_client.publish('task_channel', message)
 
@@ -32,7 +38,7 @@ def finishedArticle(uuid, time):
         'task_name': "finishedArticle",
         'article': uuid,
         "time": time,
-        'token':token
+        'token':Token().getToken()
     })
     redis_client.publish('task_channel', message)
 
@@ -41,7 +47,7 @@ def startedArticle(uuid, time):
         'task_name': "startedArticle",
         'article': uuid,
         "time": time,
-        'token':token
+        'token':Token().getToken()
     })
     redis_client.publish('task_channel', message)
 
@@ -50,7 +56,7 @@ def startedAuction(uuid, time):
         'task_name': "startedAuction",
         'article': uuid,
         "time": time,
-        'token':token
+        'token':Token().getToken()
     })
     redis_client.publish('task_channel', message)
 
@@ -59,6 +65,6 @@ def finisheddAuction(uuid, time):
         'task_name': "finishedAuction",
         'article': uuid,
         "time": time,
-        'token':token
+        'token':Token().getToken()
     })
     redis_client.publish('task_channel', message)
