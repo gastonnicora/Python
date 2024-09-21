ef5622d2958a6245d7d8131da83f03004edd19b4
gastonnicora <gastonmatias.21@gmail.com>
2024-09-21 16:31:19 -0300

    cambios por socket

--- app/socket/socketio.py
+++ app/socket/socketio.py
@@ -19,14 +19,14 @@ rooms={}
 
 @socketio.on('connect')
 def test_connect():
-    print("coneccion")
+    logging.info("coneccion")
     emit('connect', {'data': 'Connected'})
-    logging.error("conectado")
+    logging.info("conectado")
 
 @socketio.on('disconnect')
 def test_disconnect():
     try:
-        print("desconeccion")
+        logging.info("desconeccion")
         if users and users[request.sid] and users[request.sid]["room"]:
             room = users[request.sid]["room"]
             rooms[room]["users"].remove(users[request.sid])
@@ -50,21 +50,21 @@ def error_handler(e):
 
 @socketio.on_error_default
 def default_error_handler(e):
-    print(f'Default error: {e}')
+    logging.error(f'Default error: {e}')
 
 @socketio.on('borrarUser')
 def disconnect(data):
-    print("borrar user")
+    logging.info("borrar user")
     users.pop(request.sid, None)
 
 @socketio.on('coneccion')
 def test_coneccion(data):
+    logging.info("usuario conectado")
     try:
-        print("user coneccion")
         if data:
             users[request.sid]= data
-            print("coneccion data:")
-            print(data)
+            logging.info("coneccion data:")
+            logging.info(data)
             join_room(data["uuid"], request.sid)
     except Exception as e:
         logging.error("coneccion")
@@ -75,7 +75,7 @@ def test_coneccion(data):
 @socketio.on('join')
 def on_join(data):
     try:
-        print("join")
+        logging.info("join")
         room = data['room']
         join_room(room, request.sid)
         if room not in rooms:
@@ -92,38 +92,38 @@ def on_join(data):
 
 @socketio.on('leave')
 def on_leave(data):
-    print("leave")
+    logging.info("leave")
     room = data['room']
     if room in rooms and request.sid in users:
         users[request.sid]["room"]=None
         rooms[room]["users"].remove(users[request.sid])
     else:
-        print("La sala especificada no existe.")
+        logging.info("La sala especificada no existe.")
     leave_room(room, request.sid)
     emit('joinToRoom/' + room, rooms[room], room=room)
 
 def emit_bid(data):
-    print("emit bid")
+    logging.info("emit bid")
     room = data['room']
     if room in rooms and rooms[room].get("time") and rooms[room].get("time") >= 0:
         reset_countdown(room)
     socketio.emit('bidRoom/' + room, data["bid"], room=room)  
 
 def emit_finish(room):
-    print("emit finish")
+    logging.info("emit finish")
     socketio.emit('finishRoom/' + room, {'data': "finish"}, room=room)
 
 @socketio.on('leave_session')
 def on_leave_session(data):
-    print("leave")
+    logging.info("leave")
     leave_room(data["uuid"], request.sid)
 
 def emit_updateSesion(data):
-    print("update session")
+    logging.info("update session")
     socketio.emit('updateSession', {'data': data}, room=data["uuid"])
 
 def emit_start(room, time):
-    print("emit start")
+    logging.info("emit start")
     if room not in rooms:
         rooms[room] = {"users": [], "time": int(time), "timeSet": int(time), "bool": False}
     if not rooms[room].get("bool"):
@@ -135,7 +135,7 @@ def emit_start(room, time):
     socketio.emit('startRoom/' + room, room=room)
 
 def start(room):
-    print("start")
+    logging.info("start")
     if room not in rooms:
         rooms[room] = {"users": []}
     socketio.emit('startRoom/' + room, room=room)
