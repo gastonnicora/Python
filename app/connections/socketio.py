c9e4a42341756857df1dee0ecad3d6165fdcc56e
gastonnicora <gastonmatias.21@gmail.com>
2024-10-30 20:09:40 -0300

    correccion mensaje socket

--- app/connections/socketio.py
+++ app/connections/socketio.py
@@ -4,31 +4,36 @@ import json
 
 def emit_bid(data):
     message = json.dumps({
-        data
+        "data":data,
+        "task_name":"emit_bid"
     })
     redis_client.publish('task_socket_channel', message)
 
 def emit_finish(room):
     message = json.dumps({
-        "room":room
+        "room":room,
+        "task_name":"emit_finish"
     })
     redis_client.publish('task_socket_channel', message)
 
 def emit_updateSesion(data):
     message = json.dumps({
-        "data":data
+        "data":data,
+        "task_name":"emit_updateSesion"
     })
     redis_client.publish('task_socket_channel', message)
 
 def emit_start(room, time):
     message = json.dumps({
         'room':room,
-        'time':time
+        'time':time,
+        "task_name":"emit_start"
     })
     redis_client.publish('task_socket_channel', message)
 
 def start(room):
     message = json.dumps({
-        "room":room
+        "room":room,
+        "task_name":"start"
     })
     redis_client.publish('task_socket_channel', message)
